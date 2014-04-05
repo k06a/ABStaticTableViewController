fd5e4f57a9d308a76569f00f09df4699a37a1a6d
Anton Bukov <k06aaa@gmail.com>
2014-04-05 22:19:54 +0400

    Initial Commit

---
+++ ABStaticTableViewControllerTests/ABStaticTableViewControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  ABStaticTableViewControllerTests.m
+//  ABStaticTableViewControllerTests
+//
+//  Created by Антон Буков on 05.04.14.
+//  Copyright (c) 2014 Codeless Solutions. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ABStaticTableViewControllerTests : XCTestCase
+
+@end
+
+@implementation ABStaticTableViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
